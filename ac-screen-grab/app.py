382c001bd7b11ff90b51a4a37bbc42800ef0d1a5
ndled <noah@ndled.us>
2022-04-17 11:01:22 -0500

    added rq

--- ac-screen-grab/app.py
+++ ac-screen-grab/app.py
@@ -3,9 +3,14 @@ from flask import Flask, send_file, request,render_template
 import os
 import random
 
+from rq import Queue
+from rq.job import Job
+from .worker import conn
+
 from . import downloader as down
 from . import screen_cap as cap
 app = Flask(__name__)
+q = Queue(connection=conn)
 
 
 def random_image(img_dir = "./static/imgs"):
@@ -32,18 +37,35 @@ def remove_old(dir = "static/single"):
     for file in os.listdir(f"{dir}"):
         os.remove(down.resolvepath(f"{dir}/{file}"))
 
+def get_single(url):
+    remove_old()
+    down.single_download(url)
+    image = cap.single_cut()
+    return image
 
 @app.route("/single", methods=['GET', 'POST'])
 def single():
+    image = {}
     if request.method == 'POST':
         form_data = request.form
         url = form_data['new_video']
-        remove_old()
-        down.single_download(url)
-        image = cap.single_cut()
-        return render_template('single.html', image = image)
+        job = q.enqueue_call(
+            func = get_single, args = (url,),result_ttl = 5000
+        )
+        print(job.get_id())
+    return render_template('single.html', image = image)
+
+
+@app.route("/results/<job_key>", methods=['GET'])
+def get_results(job_key):
+
+    job = Job.fetch(job_key, connection=conn)
+
+    if job.is_finished:
+        return str(job.result), 200
     else:
-        return render_template('single.html')
+        return "Nay!", 202
+
 
 @app.route("/new", methods=['GET', 'POST'])
 def new():
